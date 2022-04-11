0ede5c2dfebc0631543bc11de9b54fa2fe5a9315
CheshireCatNick <CheshireCatNick@gmail.com>
2022-04-11 11:54:46 +0800

    feat: merge position columns into trader market

--- src/mappings/clearingHouse.ts
+++ src/mappings/clearingHouse.ts
@@ -70,6 +70,9 @@ export function handlePositionClosed(event: PositionClosedEvent): void {
     const traderMarket = getOrCreateTraderMarket(event.params.trader, event.params.baseToken)
     traderMarket.blockNumber = event.block.number
     traderMarket.timestamp = event.block.timestamp
+    traderMarket.takerPositionSize = traderMarket.takerPositionSize.plus(positionClosed.closedPositionSize)
+    traderMarket.openNotional = positionClosed.openNotional
+    traderMarket.entryPrice = BD_ZERO
     traderMarket.tradingVolume = traderMarket.tradingVolume.plus(abs(positionClosed.closedPositionNotional))
     traderMarket.realizedPnl = traderMarket.realizedPnl.plus(positionClosed.realizedPnl)
 
@@ -154,6 +157,13 @@ export function handlePositionChanged(event: PositionChangedEvent): void {
     const traderMarket = getOrCreateTraderMarket(event.params.trader, event.params.baseToken)
     traderMarket.blockNumber = event.block.number
     traderMarket.timestamp = event.block.timestamp
+    traderMarket.takerPositionSize = traderMarket.takerPositionSize.plus(positionChanged.exchangedPositionSize)
+    traderMarket.openNotional = positionChanged.openNotional
+    if (traderMarket.takerPositionSize.equals(BD_ZERO)) {
+        traderMarket.entryPrice = BD_ZERO
+    } else {
+        traderMarket.entryPrice = abs(traderMarket.openNotional.div(traderMarket.takerPositionSize))
+    }
     traderMarket.tradingVolume = traderMarket.tradingVolume.plus(abs(positionChanged.exchangedPositionNotional))
     traderMarket.realizedPnl = traderMarket.realizedPnl.plus(positionChanged.realizedPnl)
     traderMarket.tradingFee = traderMarket.tradingFee.plus(positionChanged.fee)
