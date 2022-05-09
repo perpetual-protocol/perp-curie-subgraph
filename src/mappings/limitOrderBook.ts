60137a562f419638dde38410ec77e7333c5c2b9f
phchan9 <phckhc@gmail.com>
2022-05-09 14:31:02 +0800

    feat: add handleLimitOrderFilled/Cancelled

--- src/mappings/limitOrderBook.ts
+++ src/mappings/limitOrderBook.ts
@@ -1,25 +1,20 @@
-import { PnlRealized as PnlRealizedEvent } from "../../generated/AccountBalance/AccountBalance"
-import { PnlRealized } from "../../generated/schema"
+import { LimitOrderFilled as LimitOrderFilledEvent, LimitOrderCancelled as LimitOrderCancelledEvent } from "../../generated/LimitOrderBook/LimitOrderBook"
+import { LimitOrderFilled, LimitOrderCancelled } from "../../generated/schema"
 import { fromWei } from "../utils/numbers"
-import { getBlockNumberLogIndex, getOrCreateProtocol, getOrCreateTrader } from "../utils/stores"
+import { getBlockNumberLogIndex, getOrCreateProtocol } from "../utils/stores"
 
-export function handlePnlRealized(event: PnlRealizedEvent): void {
-    // insert PnlRealized
-    const pnlRealized = new PnlRealized(`${event.transaction.hash.toHexString()}-${event.logIndex.toString()}`)
-    pnlRealized.blockNumberLogIndex = getBlockNumberLogIndex(event)
-    pnlRealized.blockNumber = event.block.number
-    pnlRealized.timestamp = event.block.timestamp
-    pnlRealized.txHash = event.transaction.hash
-    pnlRealized.trader = event.params.trader
-    pnlRealized.amount = fromWei(event.params.amount)
-
-    // upsert Trader
-    const trader = getOrCreateTrader(event.params.trader)
-    trader.blockNumber = event.block.number
-    trader.timestamp = event.block.timestamp
-    trader.totalPnl = trader.totalPnl.plus(pnlRealized.amount)
-    trader.settlementTokenBalance = trader.settlementTokenBalance.plus(pnlRealized.amount)
-    trader.collateral = trader.collateral.plus(pnlRealized.amount)
+export function handleLimitOrderFilled(event: LimitOrderFilledEvent): void {
+    // insert LimitOrderFilled
+    const limitOrderFilled = new LimitOrderFilled(`${event.transaction.hash.toHexString()}-${event.logIndex.toString()}`)
+    limitOrderFilled.blockNumberLogIndex = getBlockNumberLogIndex(event)
+    limitOrderFilled.blockNumber = event.block.number
+    limitOrderFilled.timestamp = event.block.timestamp
+    limitOrderFilled.txHash = event.transaction.hash
+    limitOrderFilled.trader = event.params.trader
+    limitOrderFilled.baseToken = event.params.baseToken
+    limitOrderFilled.orderHash = event.params.orderHash
+    limitOrderFilled.keeper = event.params.keeper
+    limitOrderFilled.keeperFee = fromWei(event.params.keeperFee)
 
     // upsert protocol
     const protocol = getOrCreateProtocol()
@@ -27,7 +22,27 @@ export function handlePnlRealized(event: PnlRealizedEvent): void {
     protocol.timestamp = event.block.timestamp
 
     // commit changes
-    pnlRealized.save()
-    trader.save()
+    limitOrderFilled.save()
+    protocol.save()
+}
+
+export function handleLimitOrderCancelled(event: LimitOrderCancelledEvent): void {
+    // insert LimitOrderCancelled
+    const limitOrderCancelled = new LimitOrderCancelled(`${event.transaction.hash.toHexString()}-${event.logIndex.toString()}`)
+    limitOrderCancelled.blockNumberLogIndex = getBlockNumberLogIndex(event)
+    limitOrderCancelled.blockNumber = event.block.number
+    limitOrderCancelled.timestamp = event.block.timestamp
+    limitOrderCancelled.txHash = event.transaction.hash
+    limitOrderCancelled.trader = event.params.trader
+    limitOrderCancelled.baseToken = event.params.baseToken
+    limitOrderCancelled.orderHash = event.params.orderHash
+
+    // upsert protocol
+    const protocol = getOrCreateProtocol()
+    protocol.blockNumber = event.block.number
+    protocol.timestamp = event.block.timestamp
+
+    // commit changes
+    limitOrderCancelled.save()
     protocol.save()
 }
