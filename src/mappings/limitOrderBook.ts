cefdd4142af58d204b9b8703ee6cadd6deeaabf2
Vinta Chen <vinta.chen@gmail.com>
2022-05-23 18:08:47 +0800

    add new fields for limit order events

--- src/mappings/limitOrderBook.ts
+++ src/mappings/limitOrderBook.ts
@@ -1,11 +1,16 @@
-import { LimitOrderFilled as LimitOrderFilledEvent, LimitOrderCancelled as LimitOrderCancelledEvent } from "../../generated/LimitOrderBook/LimitOrderBook"
-import { LimitOrderFilled, LimitOrderCancelled } from "../../generated/schema"
-import { fromWei } from "../utils/numbers"
-import { getBlockNumberLogIndex, getOrCreateProtocol } from "../utils/stores"
+import {
+    LimitOrderCancelled as LimitOrderCancelledEvent,
+    LimitOrderFilled as LimitOrderFilledEvent,
+} from "../../generated/LimitOrderBook/LimitOrderBook"
+import { LimitOrderCancelled, LimitOrderFilled } from "../../generated/schema"
+import { abs, fromWei } from "../utils/numbers"
+import { getBlockNumberLogIndex } from "../utils/stores"
 
 export function handleLimitOrderFilled(event: LimitOrderFilledEvent): void {
     // insert LimitOrderFilled
-    const limitOrderFilled = new LimitOrderFilled(`${event.transaction.hash.toHexString()}-${event.logIndex.toString()}`)
+    const limitOrderFilled = new LimitOrderFilled(
+        `${event.transaction.hash.toHexString()}-${event.logIndex.toString()}`,
+    )
     limitOrderFilled.blockNumberLogIndex = getBlockNumberLogIndex(event)
     limitOrderFilled.blockNumber = event.block.number
     limitOrderFilled.timestamp = event.block.timestamp
@@ -15,20 +20,22 @@ export function handleLimitOrderFilled(event: LimitOrderFilledEvent): void {
     limitOrderFilled.orderHash = event.params.orderHash
     limitOrderFilled.keeper = event.params.keeper
     limitOrderFilled.keeperReward = fromWei(event.params.keeperReward)
-
-    // upsert protocol
-    const protocol = getOrCreateProtocol()
-    protocol.blockNumber = event.block.number
-    protocol.timestamp = event.block.timestamp
+    limitOrderFilled.exchangedPositionSize = fromWei(event.params.exchangedPositionSize)
+    limitOrderFilled.exchangedPositionNotional = fromWei(event.params.exchangedPositionNotional)
+    limitOrderFilled.fee = fromWei(event.params.fee)
+    limitOrderFilled.fillPrice = abs(
+        limitOrderFilled.exchangedPositionNotional.div(limitOrderFilled.exchangedPositionSize),
+    )
 
     // commit changes
     limitOrderFilled.save()
-    protocol.save()
 }
 
 export function handleLimitOrderCancelled(event: LimitOrderCancelledEvent): void {
     // insert LimitOrderCancelled
-    const limitOrderCancelled = new LimitOrderCancelled(`${event.transaction.hash.toHexString()}-${event.logIndex.toString()}`)
+    const limitOrderCancelled = new LimitOrderCancelled(
+        `${event.transaction.hash.toHexString()}-${event.logIndex.toString()}`,
+    )
     limitOrderCancelled.blockNumberLogIndex = getBlockNumberLogIndex(event)
     limitOrderCancelled.blockNumber = event.block.number
     limitOrderCancelled.timestamp = event.block.timestamp
@@ -36,13 +43,10 @@ export function handleLimitOrderCancelled(event: LimitOrderCancelledEvent): void
     limitOrderCancelled.trader = event.params.trader
     limitOrderCancelled.baseToken = event.params.baseToken
     limitOrderCancelled.orderHash = event.params.orderHash
-
-    // upsert protocol
-    const protocol = getOrCreateProtocol()
-    protocol.blockNumber = event.block.number
-    protocol.timestamp = event.block.timestamp
+    limitOrderCancelled.positionSize = fromWei(event.params.positionSize)
+    limitOrderCancelled.positionNotional = fromWei(event.params.positionNotional)
+    limitOrderCancelled.limitPrice = abs(limitOrderCancelled.positionNotional.div(limitOrderCancelled.positionSize))
 
     // commit changes
     limitOrderCancelled.save()
-    protocol.save()
 }
