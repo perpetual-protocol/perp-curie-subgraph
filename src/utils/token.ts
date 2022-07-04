19c46387c89a1149caab24831e6a70d95fae4296
Vinta Chen <vinta.chen@gmail.com>
2022-07-05 01:44:47 +0800

    use TypedMap

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,25 +2,25 @@ import { Address, BigInt } from "@graphprotocol/graph-ts"
 import { collateralMap } from "../constants"
 
 export function fetchTokenSymbol(tokenAddress: Address): string {
-    const erc20 = collateralMap.get(tokenAddress.toHexString())
+    const erc20 = collateralMap.get(tokenAddress)
     if (!erc20) {
         return "unknown"
     }
-    return erc20.get("symbol")
+    return erc20.get("symbol")!
 }
 
 export function fetchTokenName(tokenAddress: Address): string {
-    const erc20 = collateralMap.get(tokenAddress.toHexString())
+    const erc20 = collateralMap.get(tokenAddress)
     if (!erc20) {
         return "unknown"
     }
-    return erc20.get("name")
+    return erc20.get("name")!
 }
 
 export function fetchTokenDecimals(tokenAddress: Address): BigInt {
-    const erc20 = collateralMap.get(tokenAddress.toHexString())
+    const erc20 = collateralMap.get(tokenAddress)
     if (!erc20) {
         return BigInt.fromString("18")
     }
-    return BigInt.fromString(erc20.get("decimals"))
+    return BigInt.fromString(erc20.get("decimals")!)
 }
