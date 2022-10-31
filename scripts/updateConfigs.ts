0c23727b1ce4632a570dc924ecc2d32877a6a885
Vinta Chen <vinta.chen@gmail.com>
2022-10-31 11:45:09 +0800

    fix updateConfigs

--- scripts/updateConfigs.ts
+++ scripts/updateConfigs.ts
@@ -1,18 +1,10 @@
 import DependenciesOptimismGoerli from "@perp/curie-deployments/optimism-goerli/core/dependencies.json"
 import MetadataOptimismGoerli from "@perp/curie-deployments/optimism-goerli/core/metadata.json"
-import DependenciesOptimismKovanDev1 from "@perp/curie-deployments/optimism-kovan-dev1/core/dependencies.json"
-import MetadataOptimismKovanDev1 from "@perp/curie-deployments/optimism-kovan-dev1/core/metadata.json"
-import DependenciesOptimismKovanDev2 from "@perp/curie-deployments/optimism-kovan-dev2/core/dependencies.json"
-import MetadataOptimismKovanDev2 from "@perp/curie-deployments/optimism-kovan-dev2/core/metadata.json"
 import DependenciesOptimism from "@perp/curie-deployments/optimism/core/dependencies.json"
 import MetadataOptimism from "@perp/curie-deployments/optimism/core/metadata.json"
 
 import DependenciesOptimismGoerliPeriphery from "@perp/curie-deployments/optimism-goerli/periphery/dependencies.json"
 import MetadataOptimismGoerliPeriphery from "@perp/curie-deployments/optimism-goerli/periphery/metadata.json"
-import DependenciesOptimismKovanDev1Periphery from "@perp/curie-deployments/optimism-kovan-dev1/periphery/dependencies.json"
-import MetadataOptimismKovanDev1Periphery from "@perp/curie-deployments/optimism-kovan-dev1/periphery/metadata.json"
-import DependenciesOptimismKovanDev2Periphery from "@perp/curie-deployments/optimism-kovan-dev2/periphery/dependencies.json"
-import MetadataOptimismKovanDev2Periphery from "@perp/curie-deployments/optimism-kovan-dev2/periphery/metadata.json"
 import DependenciesOptimismPeriphery from "@perp/curie-deployments/optimism/periphery/dependencies.json"
 import MetadataOptimismPeriphery from "@perp/curie-deployments/optimism/periphery/metadata.json"
 
@@ -20,46 +12,46 @@ import fs from "fs"
 
 async function main(): Promise<void> {
     const configs = [
-        {
-            name: "optimismKovanDev1",
-            network: "optimism-kovan",
-            core: {
-                ...MetadataOptimismKovanDev1,
-                ...{ version: DependenciesOptimismKovanDev1["@perp/curie-contract"] },
-            },
-            periphery: {
-                ...MetadataOptimismKovanDev1Periphery,
-                ...{ version: DependenciesOptimismKovanDev1Periphery["@perp/curie-periphery-contract"] },
-            },
-        },
-        {
-            name: "optimismKovanDev2",
-            network: "optimism-kovan",
-            core: {
-                ...MetadataOptimismKovanDev2,
-                ...{ version: DependenciesOptimismKovanDev2["@perp/curie-contract"] },
-            },
-            // we use dev1 limitOrderBook info here, will replace it after deploy to dev2
-            periphery: {
-                ...MetadataOptimismKovanDev2Periphery,
-                ...{
-                    contracts: {
-                        ...MetadataOptimismKovanDev2Periphery.contracts,
-                        LimitOrderBook: {
-                            address: "0x0D8C6A2e695F7e51C3561b8ef346f6D555175BeC",
-                            createdBlockNumber: 3051333,
-                            name: "contracts/limitOrder/LimitOrderBook.sol:LimitOrderBook",
-                        },
-                        LimitOrderRewardVault: {
-                            address: "0x9d2c256A74482399b8B8a69E50AbF7db94F2Ad0F",
-                            createdBlockNumber: 3051313,
-                            name: "contracts/limitOrder/LimitOrderRewardVault.sol:LimitOrderRewardVault",
-                        },
-                    },
-                },
-                ...{ version: DependenciesOptimismKovanDev2Periphery["@perp/curie-periphery-contract"] },
-            },
-        },
+        // {
+        //     name: "optimismKovanDev1",
+        //     network: "optimism-kovan",
+        //     core: {
+        //         ...MetadataOptimismKovanDev1,
+        //         ...{ version: DependenciesOptimismKovanDev1["@perp/curie-contract"] },
+        //     },
+        //     periphery: {
+        //         ...MetadataOptimismKovanDev1Periphery,
+        //         ...{ version: DependenciesOptimismKovanDev1Periphery["@perp/curie-periphery-contract"] },
+        //     },
+        // },
+        // {
+        //     name: "optimismKovanDev2",
+        //     network: "optimism-kovan",
+        //     core: {
+        //         ...MetadataOptimismKovanDev2,
+        //         ...{ version: DependenciesOptimismKovanDev2["@perp/curie-contract"] },
+        //     },
+        //     // we use dev1 limitOrderBook info here, will replace it after deploy to dev2
+        //     periphery: {
+        //         ...MetadataOptimismKovanDev2Periphery,
+        //         ...{
+        //             contracts: {
+        //                 ...MetadataOptimismKovanDev2Periphery.contracts,
+        //                 LimitOrderBook: {
+        //                     address: "0x0D8C6A2e695F7e51C3561b8ef346f6D555175BeC",
+        //                     createdBlockNumber: 3051333,
+        //                     name: "contracts/limitOrder/LimitOrderBook.sol:LimitOrderBook",
+        //                 },
+        //                 LimitOrderRewardVault: {
+        //                     address: "0x9d2c256A74482399b8B8a69E50AbF7db94F2Ad0F",
+        //                     createdBlockNumber: 3051313,
+        //                     name: "contracts/limitOrder/LimitOrderRewardVault.sol:LimitOrderRewardVault",
+        //                 },
+        //             },
+        //         },
+        //         ...{ version: DependenciesOptimismKovanDev2Periphery["@perp/curie-periphery-contract"] },
+        //     },
+        // },
         {
             name: "optimismGoerli",
             network: "optimism-goerli",
