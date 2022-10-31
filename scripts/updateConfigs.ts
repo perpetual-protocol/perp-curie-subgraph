53e0c2339d596fc312d91c13c6ca4a6dec0eb033
Vinta Chen <vinta.chen@gmail.com>
2022-10-31 18:26:27 +0800

    read referral contract address from periphery metadata

--- scripts/updateConfigs.ts
+++ scripts/updateConfigs.ts
@@ -12,46 +12,6 @@ import fs from "fs"
 
 async function main(): Promise<void> {
     const configs = [
-        // {
-        //     name: "optimismKovanDev1",
-        //     network: "optimism-kovan",
-        //     core: {
-        //         ...MetadataOptimismKovanDev1,
-        //         ...{ version: DependenciesOptimismKovanDev1["@perp/curie-contract"] },
-        //     },
-        //     periphery: {
-        //         ...MetadataOptimismKovanDev1Periphery,
-        //         ...{ version: DependenciesOptimismKovanDev1Periphery["@perp/curie-periphery-contract"] },
-        //     },
-        // },
-        // {
-        //     name: "optimismKovanDev2",
-        //     network: "optimism-kovan",
-        //     core: {
-        //         ...MetadataOptimismKovanDev2,
-        //         ...{ version: DependenciesOptimismKovanDev2["@perp/curie-contract"] },
-        //     },
-        //     // we use dev1 limitOrderBook info here, will replace it after deploy to dev2
-        //     periphery: {
-        //         ...MetadataOptimismKovanDev2Periphery,
-        //         ...{
-        //             contracts: {
-        //                 ...MetadataOptimismKovanDev2Periphery.contracts,
-        //                 LimitOrderBook: {
-        //                     address: "0x0D8C6A2e695F7e51C3561b8ef346f6D555175BeC",
-        //                     createdBlockNumber: 3051333,
-        //                     name: "contracts/limitOrder/LimitOrderBook.sol:LimitOrderBook",
-        //                 },
-        //                 LimitOrderRewardVault: {
-        //                     address: "0x9d2c256A74482399b8B8a69E50AbF7db94F2Ad0F",
-        //                     createdBlockNumber: 3051313,
-        //                     name: "contracts/limitOrder/LimitOrderRewardVault.sol:LimitOrderRewardVault",
-        //                 },
-        //             },
-        //         },
-        //         ...{ version: DependenciesOptimismKovanDev2Periphery["@perp/curie-periphery-contract"] },
-        //     },
-        // },
         {
             name: "optimismGoerli",
             network: "optimism-goerli",
@@ -63,12 +23,6 @@ async function main(): Promise<void> {
                 ...MetadataOptimismGoerliPeriphery,
                 ...{ version: DependenciesOptimismGoerliPeriphery["@perp/curie-periphery-contract"] },
             },
-            // referral: {
-            //     contracts: {
-            //         PerpetualProtocolReferrer: "0x5613A1522Ee8CFC38a036d51e8Dc7a5273301969",
-            //         PerpetualProtocolReferrerStartBlock: 7648933, // PerpetualProtocolReferrer createdBlockNumber
-            //     },
-            // },
         },
         {
             name: "optimism",
@@ -80,12 +34,6 @@ async function main(): Promise<void> {
             periphery: {
                 ...MetadataOptimismPeriphery,
                 ...{ version: DependenciesOptimismPeriphery["@perp/curie-periphery-contract"] },
-            },
-            referral: {
-                contracts: {
-                    PerpetualProtocolReferrer: "0xbfBa3368d94b8b006A4fd59C13b9e9F9b071D106",
-                    PerpetualProtocolReferrerStartBlock: 513591, // ClearingHouse createdBlockNumber
-                },
             },
             // NOTE: subgraph id exists in The Graph might not exist in self-hosted graph node,
             // so we might need to set different graft.base
